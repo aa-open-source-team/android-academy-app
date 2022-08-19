0d0743eebb1ee1658b778e6fb039e908c453dbc8
mik629 <mikhail.id.123@gmail.com>
2022-08-19 11:09:00 +0400

    Upgraded Kotlin and the libs to latest stable versions

--- buildSrc/src/main/java/config/Versions.kt
+++ buildSrc/src/main/java/config/Versions.kt
@@ -1,35 +1,34 @@
 package config
 
 object Versions {
-    const val kotlin = "1.6.21"
-    const val activityCompose = "1.4.0"
-    const val compose = "1.2.0-rc02"
-    const val vmCompose = "2.4.0-rc01"
-    const val navigationCompose = "2.4.0-alpha10"
-    const val hiltNavigationCompose = "1.0.0-rc01"
-    const val lifecycle = "2.4.0"
+    const val kotlin = "1.7.0"
+    const val activityCompose = "1.5.1"
+    const val compose = "1.2.0"
+    const val navigationCompose = "2.5.1"
+    const val hiltNavigationCompose = "1.0.0"
+    const val lifecycle = "2.5.1"
     const val workManager = "2.7.1"
-    const val material3 = "1.0.0-alpha04"
+    const val material3 = "1.0.0-alpha16"
 
     // Plugins
-    const val androidToolsPlugin = "7.0.4"
-    const val versionsPlugin = "0.41.0"
-    const val detekt = "1.19.0"
+    const val androidToolsPlugin = "7.2.2"
+    const val versionsPlugin = "0.42.0"
+    const val detekt = "1.21.0"
 
     // Android libraries
-    const val androidCore = "1.7.0"
-    const val coroutines = "1.6.0"
-    const val dagger = "2.40.5"
+    const val androidCore = "1.8.0"
+    const val coroutines = "1.6.4"
+    const val dagger = "2.43.2"
     const val dataStore = "1.0.0"
-    const val playServicesAuth = "20.0.1"
+    const val playServicesAuth = "20.2.0"
     const val serialization = "1.3.3"
 
     // third party libs
-    const val glideCompose = "1.4.5"
+    const val glideCompose = "1.6.1"
     const val timber = "5.0.1"
 
     // testing
     const val junit = "4.13.2"
     const val junitExt = "1.1.3"
-    const val kaspresso = "1.4.0"
+    const val kaspresso = "1.4.1"
 }
